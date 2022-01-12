521bfaf2770be6ff1a92b746eabf6db78244f291
diveshsrivastava <srivastavakdivesh@gmail.com>
2022-01-12 17:47:08 +0530

    Initial Project Commit

---
+++ Blockchain-Capstone-master/eth-contracts/migrations/2_deploy_contracts.js
@@ -0,0 +1,8 @@
+var SquareVerifier = artifacts.require("verifier.sol");
+var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+
+module.exports = async function(deployer) 
+{
+  await deployer.deploy(SquareVerifier);
+  await deployer.deploy(SolnSquareVerifier, SquareVerifier.address, "DIVToken", "DIV");
+};
---
+++ Blockchain-Capstone-master/eth-contracts/truffle-config.js
@@ -0,0 +1,57 @@
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * truffleframework.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura API
+ * keys are available for free at: infura.io/register
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
+const HDWallet = require('truffle-hdwallet-provider');
+
+module.exports = {
+ 
+  networks: {
+   
+     development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     },
+
+     rinkeby: {
+			provider: function () {
+				return new HDWallet("",
+				"https://rinkeby.infura.io/v3/cbf3404cad2c4d03af134fd3ee27c701",
+				)
+			},
+			network_id: '4',
+			gas: 4500000,
+			gasPrice: 10000000000,
+		}
+    
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      
+    }
+  }
+}
